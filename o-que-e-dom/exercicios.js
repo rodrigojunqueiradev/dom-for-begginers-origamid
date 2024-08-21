6974ce1627572dbb46f4682009f3d02d2fc549e4
Rodrigo Junqueira <eurodrigojunqueira@gmail.com>
2024-08-21 17:04:50 -0300

    O que é o DOM?

---
+++ o-que-e-dom/exercicios.js
@@ -0,0 +1,15 @@
+// Retorne o url da página atual utilizando o objeto window
+const urlAtual = window.location.href;
+console.log(urlAtual);
+
+// Seleciona o primeiro elemento da página que possua a classe ativo
+const elementoAtivo = document.querySelector(".ativo");
+console.log(elementoAtivo);
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+
+// Retorne a largura da janela
+const larguraJanela = window.innerWidth;
+console.log(larguraJanela);
--- o-que-e-dom/script.js
+++ o-que-e-dom/script.js
@@ -1,2 +1,14 @@
-console.log(window);
-console.log(window.innerHeight);
+// window.alert("Isso é um alerta");
+// alert("Isso também é um alerta");
+
+const href = window.location.href;
+
+const titulo = document.querySelector("h1");
+const tituloClasses = titulo.classList;
+const tituloId = titulo.id;
+
+function callbackH1() {
+  console.log("Clicou em ", titulo);
+}
+
+titulo.addEventListener("click", callbackH1);
